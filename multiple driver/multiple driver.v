9c4dfc2bdf67a00b04ac6bf2153238ba1879434f
NAVEEN R.S <127390244+nrs14@users.noreply.github.com>
2025-11-21 16:16:35 +0530

    Create multiple driver.v

---
+++ multiple driver/multiple driver.v
@@ -0,0 +1,14 @@
+/*Multiple Drivers
+Drive a single output y from two different inputs a and b using two separate continuous assignments. Observe the result when a and b differ.
+Requirements:
+Module Name: top_module
+Inputs: a, b (1-bit each)
+Outputs: y (1-bit)*/
+-------------------------------------------------
+module top_module(
+    input a,
+    input b,   output y);
+assign y = a;
+assign y = b;
+endmodule
+------------------------------------------------
